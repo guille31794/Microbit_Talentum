fea7928ed5fa752ef7fd6fe9e88c22f68fc6a820
Guillermo <guille.g94@hotmail.es>
2021-02-22 17:52:06 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,25 @@
 input.onButtonPressed(Button.A, function () {
     music.playTone(523, music.beat(BeatFraction.Whole))
     music.stopAllSounds()
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 39)
-    while (true) {
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P12, 1)
+    while (5 > maqueen.Ultrasonic(PingUnit.Centimeters)) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 39)
         basic.showString("Stop")
         basic.showArrow(ArrowNames.East)
     }
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 25)
 })
 input.onButtonPressed(Button.B, function () {
     maqueen.motorStop(maqueen.Motors.All)
     music.playTone(131, music.beat(BeatFraction.Whole))
     music.stopAllSounds()
-    basic.showArrow(ArrowNames.West)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    while (true) {
+        basic.showArrow(ArrowNames.West)
+        basic.showString("Move")
+    }
 })
 basic.showString("Hello!")
 basic.pause(1000)
